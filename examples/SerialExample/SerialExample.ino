a65c20e874a7a5132b4b72259e1e0624e890072f
Matheus Andrade <matheus.amazonas@gmail.com>
2018-08-19 11:43:09 +0200

    Updated example to handle invalid data

--- examples/SerialExample/SerialExample.ino
+++ examples/SerialExample/SerialExample.ino
@@ -5,19 +5,26 @@ DHTino dht(4);
 void setup()
 {
 	Serial.begin(9600);
-  while (!Serial) 
-  {
-  }
-  dht.begin();
-  Serial.println("Setup is done");
+	while (!Serial) 
+	{
+	}
+	dht.begin();
+	Serial.println("Setup is done");
 }
 
 void loop()
 {
-  delay(2000);
-  struct DHTinfo info = dht.getInfo();
-  Serial.print("Humidity: ");
-  Serial.println(info.humid);
-  Serial.print("Temperature: ");
-  Serial.println(info.temp);
+	delay(2000);
+	struct DHTinfo info = dht.getInfo();
+	if (info.valid)
+	{
+		Serial.print("Humidity: ");
+		Serial.println(info.humid);
+		Serial.print("Temperature: ");
+		Serial.println(info.temp);
+	}
+	else
+	{
+		Serial.println("Info is invalid");
+	}
 }
